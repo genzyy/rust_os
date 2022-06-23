83005575ed133f2e8fbfef3116d3df5987ff70a1
Rishit <rishpandey8097@gmail.com>
2022-06-23 18:56:56 +0530

    Add: heap allocator

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@
 
 extern crate alloc;
 
-use alloc::boxed::Box;
+use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use rust_os::println;
+use rust_os::{allocator, println};
 
 // instead of defining our own start function, using pub extern C, we use entry_point function caller
 // provided by bootiamge crate, so we know what type of function with what arguments should the
@@ -27,36 +27,62 @@ fn kernel_boot(boot_info: &'static BootInfo) -> ! {
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
     // map an unused page
-    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+    //let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    //memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
     // write the string `New!` to the screen through the new mapping.
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
+    //let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    //unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
-    let addresses = [
-        // the identity-mapped vga buffer page
-        0xb8000,
-        // some code page
-        0x201008,
-        // some stack page
-        0x0100_0020_1a10,
-        // virtual address mapped to physical address 0
-        boot_info.physical_memory_offset,
-    ];
+    //let addresses = [
+    // the identity-mapped vga buffer page
+    //0xb8000,
+    // some code page
+    //0x201008,
+    // some stack page
+    //  0x0100_0020_1a10,
+    // virtual address mapped to physical address 0
+    //  boot_info.physical_memory_offset,
+    //];
 
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = mapper.translate_addr(virt);
-        println!("{:?} -> {:?}", virt, phys);
+    //for &address in &addresses {
+    //let virt = VirtAddr::new(address);
+    // let phys = mapper.translate_addr(virt);
+    //  println!("{:?} -> {:?}", virt, phys);
 
-        // the last 12 bits of the addresses remain same
-        // because they are page offset and are not
-        // part of translation.
+    // the last 12 bits of the addresses remain same
+    // because they are page offset and are not
+    // part of translation.
+    //}
+
+    // allocate a number on the heap.
+    let heap_value = Box::new(41);
+    println!("heap value at {:p}", heap_value);
+
+    // create a dynamically sized vector.
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
     }
 
-    let x = Box::new(41);
+    println!("vec at {:p}", vec.as_slice());
+
+    // create a reference counted vector -> will be freed when count reaches 0.
+    // rc -> Reference Counted -> reference counting pointer.
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!(
+        "current reference count is {}",
+        Rc::strong_count(&cloned_reference)
+    );
+    core::mem::drop(reference_counted);
+    println!(
+        "reference count is {} now",
+        Rc::strong_count(&cloned_reference)
+    );
 
     #[cfg(test)]
     test_main();
